119882f7ccfaa8eb25566783c1edbc7dbc2a9d29
Steve Chan <chanshunli@gmail.com>
2018-04-27 18:39:02 +0800

    放入第一个Clojure文件

---
+++ app/src/main/clojure/plain/someactivity.clj
@@ -0,0 +1,31 @@
+(ns plain.someactivity
+  (:import
+            (android.support.v7.app AppCompatActivity)
+            (android.util Log)
+            (com.example.stevechan.clojurecppapplication.R$layout))
+            (:require [org.httpkit.client :as http])
+
+  (:gen-class
+    :name "plain.someactivity.MyActivity"
+    :exposes-methods {onCreate superOnCreate}
+     :extends android.support.v7.app.AppCompatActivity
+     :prefix "some-")
+    )
+
+(defn fetch [url]
+  (http/get url))
+
+(defn some-onCreate [^plain.someactivity.MyActivity this ^android.os.Bundle bundle]
+    (.superOnCreate this bundle)
+    (.setContentView this com.example.stevechan.clojurecppapplication.R$layout/activity_main)
+    ;;(future (let [data (fetch "http://www.yahoo.co.jp")]
+    ;;                      (Log/i "clojure" (str data))))
+
+
+  (.start (Thread. (fn []
+            (let [data (fetch "http://www.yahoo.co.jp")]
+                              (Log/i "clojure" (:body @data))))
+
+              ))
+
+    )
