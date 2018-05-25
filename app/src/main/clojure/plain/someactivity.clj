c3cf9ca12517244375078e3ef232e98abc7b5ecb
Steve Chan <chanshunli@gmail.com>
2018-05-25 21:37:47 +0800

    格式化someactivity.clj

--- app/src/main/clojure/plain/someactivity.clj
+++ app/src/main/clojure/plain/someactivity.clj
@@ -1,31 +1,25 @@
 (ns plain.someactivity
   (:import
-            (android.support.v7.app AppCompatActivity)
-            (android.util Log)
-            ;;(com.example.stevechan.clojurecppapplication.R$layout)
-            (com.example.stevechan.clojurecppapplication.R$id)
-            (com.example.stevechan.clojurecppapplication.R$layout)
-            )
-            (:require [org.httpkit.client :as http])
-
+   (android.support.v7.app AppCompatActivity)
+   (android.util Log)
+   (com.example.stevechan.clojurecppapplication.R$id)
+   (com.example.stevechan.clojurecppapplication.R$layout))
+  (:require [org.httpkit.client :as http])
   (:gen-class
-    :name "plain.someactivity.MyActivity"
-    :exposes-methods {onCreate superOnCreate}
-     :extends android.support.v7.app.AppCompatActivity
-     :prefix "some-")
-    )
+   :name "plain.someactivity.MyActivity"
+   :exposes-methods {onCreate superOnCreate}
+   :extends android.support.v7.app.AppCompatActivity
+   :prefix "some-"))
 
 (defn fetch [url]
   (http/get url))
 
 (defn some-onCreate [^plain.someactivity.MyActivity this ^android.os.Bundle bundle]
-    (.superOnCreate this bundle)
-    (.setContentView this com.example.stevechan.clojurecppapplication.R$layout/activity_main)
-
-  (.start (Thread. (fn []
-            (let [data (fetch "http://www.yahoo.co.jp")]
-                              (Log/i "clojure" (:body @data))))
-
-              ))
-
-    )
+  (.superOnCreate this bundle)
+  (.setContentView this com.example.stevechan.clojurecppapplication.R$layout/activity_main)
+  (Log/i "clojure" "已启动应用...")
+  (System/loadLibrary "native-lib")
+  #_(.start (Thread. (fn []
+                       (let [data (fetch "http://www.yahoo.co.jp")]
+                         (Log/i "clojure" (:body @data))))))
+  )
